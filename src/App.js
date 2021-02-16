457d7982d5541ef2524d333a2bbfbbaa4b2a4765
Yuehao <pandaxfly@gmail.com>
2021-02-16 22:19:15 +0800

    Imported Tesseract and developed basic file read and recognize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Tesseract  from '../node_modules/tesseract.js'
 
 function App() {
+
+  function handleChange(e) {
+    Tesseract.recognize(
+      e.target.files[0],
+      'eng',{ 
+      logger: m => console.log(m) 
+    }).then(({ data: { text } }) => {
+      console.log(text);
+      document.getElementById('result').innerHTML = text;
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="file" onChange={handleChange}></input>
+        <pre id='result'></pre>
       </header>
+      
     </div>
   );
 }
