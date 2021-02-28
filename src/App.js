c8253b957db60dff3932e35c6aa0d42badbfedfa
Yuehao <pandaxfly@gmail.com>
2021-03-01 00:03:13 +0800

    converted to react component and added vision API

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Tesseract  from '../node_modules/tesseract.js'
+import Tesseract  from '../node_modules/tesseract.js';
 
-function App() {
+class App extends React.Component {
+  //let uploadedFile = null;
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
 
-  function handleChange(e) {
+  handleChange = (e) => {
+    // Call Vision API
+    const formData = new FormData();
+    formData.append('fileContent', e.target.files[0], 'filename.jpg');
+    formData.append('otherdata', 'test.jpg');
+
+    const axios = require('axios');
+    axios.post('http://localhost:8081/api/upload', formData)
+    .then(res => { // then print response status
+      console.log(res)
+      document.getElementById('result_vision').innerHTML = res.data.text;
+    });
+
     // Frontend Tesseract Result
     Tesseract.recognize(e.target.files[0], 'eng',{ 
       logger: m => document.getElementById('status').innerHTML = m.status
     })
     .then(({ data: { text } }) => {
-      document.getElementById('result').innerHTML = text;
-    });    
+      document.getElementById('result_tess').innerHTML = text;
+    });
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <input type="file" onChange={handleChange} />
-        <p id='status'></p>
-        <br />
-        <p id='result'></p>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <input type="file" onChange={this.handleChange} /> 
+          <br />
+          <h3> Tesseract JS Result:</h3>
+          <p id='status'></p>
+          <br />
+          <p id='result_tess'></p>
+          <br />
+          <h3> Vision API Result:</h3>
+          <p id='result_vision'></p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
